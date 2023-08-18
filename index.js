1e94d6199e00bc772797edfbb0de79d103a7de70
bhabna01 <abierhoque01@gmail.com>
2023-08-19 01:58:24 +0600

    2nd commit

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const userRoutes = require('./routes/userRoutes');
 app.use(bodyParser.json());
 
 app.use('/user', userRoutes);
-
+app.use('/', () => {
+  console.log("Server is running");
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
